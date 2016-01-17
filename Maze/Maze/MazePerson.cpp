a6166682e1a72a4e05754af897c411b7a566237d
ZivenKan <gmtg90@gmail.com>
2016-01-17 19:12:14 +0800

    V2.0

--- Maze/Maze/MazePerson.cpp
+++ Maze/Maze/MazePerson.cpp
@@ -2,26 +2,25 @@
 #include <iostream>
 using namespace std;
 
+//宏定义地图
 #define WALL 0
 #define ROAD 1
 
+//宏定义方向
 #define UP 0
 #define LEFT 1
 #define DOWN 2
 #define RIGHT 3
 
-#define FAST 1
-#define SLOW 0
+const int MazePerson::m_Next[4][2] = {{-1, 0},{0, -1},{1, 0},{0, 1}};
 
-int success = 0;
-
+//重载符号函数，比较两个Position对象的坐标是否相同
 bool operator==(Position& p1, Position &p2)
 {
 	if (p1.getX() == p2.getX() && p1.getY() == p2.getY())
 	{
 		return true;
 	}
-
 	return false;
 }
 
@@ -44,6 +43,7 @@ void MazePerson::setPersonChar(char shpe)
 	m_cPersonShap = shpe;
 }
 
+//设置光标指向位置
 void MazePerson::gotoxy(int x,int y)
 {
 	cout << char(8) << " ";
@@ -61,63 +61,65 @@ void MazePerson::gotoxy(int x,int y)
 void MazePerson::start()
 {
 	int exDir;
-	int exX;
-	int exY;
 	int curX;
 	int curY;
-	int next[4][2] = {{-1, 0},{0, -1},{1, 0},{0, 1}};
+	/*int next[4][2] = {{-1, 0},{0, -1},{1, 0},{0, 1}}; //下一位置*/
 
 	//获取初始点坐标
-	success++;
+	m_iSteps = 0;
 	int initX = m_stPosition.getX();
 	int initY = m_stPosition.getY();
 	gotoxy(m_stPosition.getX(), m_stPosition.getY());
 
-	//得到第两个点
+	//得到第二个点
 	m_exPosition = m_stPosition;
-	int i = RIGHT;
-	while (m_imap.getMazeMap(initX + next[i][0],initY + next[i][1]) != WALL
-		|| m_imap.getMazeMap(initX + next[(i+1)%4][0], initY + next[(i+1)%4][1]) != ROAD)
+	int i = RIGHT;	//×第一步存在随机性，而且局限大
+	while (m_imap.getMazeMap(initX + m_Next[i][0],initY + m_Next[i][1]) != WALL
+		|| m_imap.getMazeMap(initX + m_Next[(i+1)%4][0], initY + m_Next[(i+1)%4][1]) != ROAD)
 	{
 		i = (i + 1)%4;
 	}
-	m_iPersonDir = (i+1)%4;
-	curX = m_exPosition.getX() + next[m_iPersonDir][0];
-	curY = m_exPosition.getY() + next[m_iPersonDir][1];
-	m_curPosition.setX(curX);
-	m_curPosition.setY(curY); 
-	success++;
-	gotoxy(m_curPosition.getX(), m_curPosition.getY());
+	m_iCurDir = (i+1)%4;
+	m_curPosition = toNextPositon(m_iCurDir);
+	curX = m_curPosition.getX();
+	curY = m_curPosition.getY();
+	m_iSteps++;
 
 	//得到第三个到最后一个点
 	while (curX > 0 && curY > 0 && curX < m_imap.getRow()-1 && curY < m_imap.getColumn()-1)
 	{
-		exDir = m_iPersonDir;
+		exDir = m_iCurDir;
 		m_exPosition = m_curPosition;
 		int i = exDir - 1 + 4;
 
-		while(m_imap.getMazeMap(m_exPosition.getX() + next[i%4][0], m_exPosition.getY() + next[i%4][1])!= ROAD)
+		while(m_imap.getMazeMap(m_exPosition.getX() + m_Next[i%4][0], m_exPosition.getY() + m_Next[i%4][1])!= ROAD)
 		{
 			i++;
 		}
-		m_iPersonDir = i;
-		curX = m_exPosition.getX() + next[i%4][0];
-		curY = m_exPosition.getY() + next[i%4][1];
-		m_curPosition.setX(curX);
-		m_curPosition.setY(curY);
-		success++;
-		gotoxy(m_curPosition.getX(), m_curPosition.getY());
+		m_iCurDir = i;
+		m_curPosition = toNextPositon(m_iCurDir);
+		curX = m_curPosition.getX();
+		curY = m_curPosition.getY();
+		m_iSteps++;
 	}
 	gotoxy(m_imap.getRow()+1,0);
 	if (!(m_curPosition == m_stPosition))
 	{
 		cout << "开心地说：哈哈，我出来咯～～" << endl;
-		cout << "（总共走了" << success << "步）" << endl;
+		cout << "（总共走了" << m_iSteps << "步）" << endl;
 	}
 	else if(m_curPosition == m_stPosition)
 	{
 		cout << "生气地说：耍我咯！！！！" << endl;
-		cout << "（总共走了" << success << "步）" << endl;
+		cout << "（总共走了" << m_iSteps << "步）" << endl;
 	}
 }      
 
+Position MazePerson::toNextPositon(int dir)
+{
+	/*int next[4][2] = {{-1, 0},{0, -1},{1, 0},{0, 1}}; //下一位置*/
+	m_curPosition.setX(m_exPosition.getX() + m_Next[dir%4][0]);
+	m_curPosition.setY(m_exPosition.getY() + m_Next[dir%4][1]);
+	gotoxy(m_curPosition.getX(), m_curPosition.getY());
+	return m_curPosition;
+}
--- Maze/Maze/MazePerson.h
+++ Maze/Maze/MazePerson.h
@@ -8,21 +8,29 @@ public:
 	// 给人一个地图
 	MazePerson(int x,int y, MazeMap &map) : m_imap(map), m_curPosition(x, y), m_exPosition(x, y), m_stPosition(x, y), m_iSpeed(1000)
 	{
+		//需要每个对象一份时
+// 		int tmp[][2] = {{-1, 0},{0, -1},{1, 0},{0, 1}};
+// 		memcpy(&m_Next[0][0], tmp, sizeof(tmp));
 
 	}
 	void setPersonPosition(int x, int y);
 	void setPersonSpeed(int spd);
 	void setPersonChar(char shpe);
-	void start();
+	Position toNextPositon(int dir);
 	void gotoxy(int x,int y);
+	void start();
+
 
 private:
 
 	char m_cPersonShap;
-	int m_iPersonDir;
+	int m_iExDir;
+	int m_iCurDir;
 	int m_iSpeed;
+	int m_iSteps;
 	Position m_stPosition;
 	Position m_exPosition;
 	Position m_curPosition;
 	MazeMap &m_imap;
+	static const int m_Next[4][2]; //此类中只有一份	
 };
